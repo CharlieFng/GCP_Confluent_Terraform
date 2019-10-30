7ff45e2c753a5ea279d7199b0fb6d0611442e7e4
Charlie <charlie@192-168-1-11.tpgi.com.au>
2019-10-30 21:16:57 +1100

    adjust script

--- confluent/start-all.sh
+++ confluent/start-all.sh
@@ -1,5 +1,1 @@
-#terraform init gke/ && terraform plan gke/ && terraform apply -auto-approve gke/ 
 
-terraform init kubernetes/ && terraform plan kubernetes/ && terraform apply -auto-approve kubernetes/ 
-
-#terraform init helm/ && terraform plan helm/ && terraform apply -auto-approve helm/ 
